220e5e5196d66317ebf011fb26fa71f2d11d012b
JorgeRomanArtes <114657979+JorgeRomanArtes@users.noreply.github.com>
2022-11-04 09:33:07 +0100

    comit Inicial :tada:

---
+++ src/javaapplication36/JavaApplication36.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package javaapplication36;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jorge Román Artes - jorromart@alu.edu.gva.es - Jorge Román
+ */
+public class JavaApplication36 {
+
+    
+    public static void main(String[] args) {
+      
+      long  Hola = new Date().getTime();  
+      
+        System.out.printf("Tiempo UNIX .........:%d%n", Hola);
+        
+        
+        
+        
+        
+    }
+
+}
